d4ec70241670600588de5dd768c5065f001aa068
alekseishmidko <118507792+alekseishmidko@users.noreply.github.com>
2024-06-23 23:05:06 +0300

    31:00

---
+++ src/user/user.controller.ts
@@ -0,0 +1,24 @@
+import { Controller, Get, HttpCode, Param, Patch } from '@nestjs/common';
+import { UserService } from './user.service';
+import { CurrentUser } from 'src/auth/decorators/user.decorator';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+
+@Controller('user')
+export class UserController {
+	constructor(private readonly userService: UserService) {}
+	@Get('profile')
+	@Auth()
+	async getProfile(@CurrentUser('id') id: string) {
+		return this.userService.getById(id);
+	}
+
+	@HttpCode(200)
+	@Auth()
+	@Patch('profile/favorites/:productId')
+	async toggleFavorite(
+		@CurrentUser('id') id: string,
+		@Param('productId') productId: string
+	) {
+		return this.userService.toggleFavorite(id, productId);
+	}
+}
