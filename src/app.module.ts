22a4ae79c6f7337000e6337df6ae059466f5ffac
alekseishmidko <118507792+alekseishmidko@users.noreply.github.com>
2024-06-24 23:57:30 +0300

    43

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,19 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
+import { CategoryModule } from './category/category.module';
+import { OrderModule } from './order/order.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
-	imports: [AuthModule, UserModule, ConfigModule],
+	imports: [
+		AuthModule,
+		UserModule,
+		ConfigModule,
+		CategoryModule,
+		OrderModule,
+		ProductModule
+	],
 	controllers: [AppController],
 	providers: [AppService]
 })
